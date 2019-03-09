10b5b92a89ca890f500b516c537b5c7804142fc1
dev-239 <47806537+dev-239@users.noreply.github.com>
2019-03-09 21:19:51 +0100

    20190309 v3 (#13)
    * Updated and moved act-bootstrap.sh
    
    - Moved script into scripts directory
    - Added logging of events
    - Moved logs produced by python scripts to log/ directory
    - Added more checks for missing cli args
    
    * Updated requirements.txt
    
    * Updated README to reflect changes
    
    * Added .gitignore to avoid logs and env directories being pushed

--- README.md
+++ README.md
@@ -4,26 +4,28 @@
 
 These scripts are used to bootstrap the [ACT](https://github.com/mnemonic-no/act-platform) data model (object types and fact types) and open sources, like mitre attack and threat actors from MISP galaxy.
 
-## Requirements
-
-* [act-api-python](https://github.com/mnemonic-no/act-api-python)
+## Installation
+1. This project requires that you have a running installation of the [act-platform](https://github.com/mnemonic-no/act-platform).
+2. To install the requirements and get this project running perform the following:
+```
+virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+```
 
 ## Usage
+To bootstrap the type system and all OSINT (using userID 1 and API server on localhost:8888:
+```
+./scripts/act-bootstrap.sh 1 http://localhost:8888
+```
 
 To bootstrap only the types:
-
 ```
 bootstrap/act-bootstrap.py \
     --userid 1 \
-    --act-baseurl http://localhost:8080 \
+    --act-baseurl http://localhost:8888 \
     --loglevel ERROR \
     --object-types types/object-types.json \
     --fact-types types/fact-types.json \
     --meta-fact-types types/metafact-types.json
-```
-
-To bootstrap the type system and all OSINT (using userID 1 and API server on localhost:8080):
-
-```
-./act-bootstrap.sh 1 http://localhost:8080
 ```
---
+++ scripts/act-bootstrap.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+set -e
+
+function log {
+    echo "`date` [BOOTSTRAP] $1"
+}
+
+function usage {
+    echo ""
+    echo "syntax: bootstrap.sh <user id> <act baseurl>"
+    echo "example: bootstrap.sh 1 http://localhost:8888"
+}
+
+BOOTSTRAP_HOME="`dirname $0`/.."
+USERID=$1
+ACT_BASEURL=$2
+LOGLEVEL=info
+LOGDIR=${BOOTSTRAP_HOME}/log
+LOGFILE=${LOGDIR}/bootstrap.log.`date +%s`
+FACT_TYPES=${BOOTSTRAP_HOME}/types/fact-types.json
+META_FACT_TYPES=${BOOTSTRAP_HOME}/types/metafact-types.json
+OBJECT_TYPES=${BOOTSTRAP_HOME}/types/object-types.json
+
+if [ ! -d "$LOGDIR" ]; then
+    log "Created log directory $LOGDIR"
+    echo $LOGDIR
+    mkdir ${LOGDIDR}
+fi
+
+if [ "$USERID" == "" ]; then
+    log "Please supply an ACT user id"
+    usage
+    exit 1
+fi
+
+if [ "$ACT_BASEURL" == "" ]; then
+    log "Please supply an ACT base url"
+    usage
+    exit 1
+fi
+
+export PYTHONPATH=$PYTHONPATH:${BOOTSTRAP_HOME}/bootstrap
+
+ARGS="--userid $USERID --act-baseurl $ACT_BASEURL --loglevel $LOGLEVEL --logfile $LOGFILE"
+
+log "Starting bootstrap process, logging to $LOGFILE"
+bootstrap/act-bootstrap.py $ARGS --object-types ${OBJECT_TYPES} --fact-types ${FACT_TYPES} --meta-fact-types ${META_FACT_TYPES}
+bootstrap/misp-threat-actors.py $ARGS
+bootstrap/fireeye-carbanak.py $ARGS
+log "Bootstraping completed"
